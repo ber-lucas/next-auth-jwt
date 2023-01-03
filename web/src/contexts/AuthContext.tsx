7631a94d49bdd9a184af0e72ac2f2e7aee81aec3
ber-lucas <bernardo.araujo@aluno.ufop.edu.br>
2023-01-02 22:35:04 -0300

    dados do user armazenados em estado e repassado para o provider

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
-import { setCookie } from 'nookies';
-import { createContext } from "react";
+import Router from 'next/router';
+import { parseCookies, setCookie } from 'nookies';
+import { createContext, useEffect, useState } from "react";
 import { signInRequest } from "../services/auth";
 
 type SignInDataType = {
@@ -7,26 +8,48 @@ type SignInDataType = {
   password: string,
 }
 
+type User = {
+  name: string,
+  email: string,
+  avatar_url: string,
+}
+
 type AuthContextType = {
   isAuthenticated: boolean,
-  signIn: (data: SignInDataType) => Promise<void>
+  user?: User,
+  signIn: (data: SignInDataType) => Promise<void>,
 }
 
 export const AuthContext = createContext({} as AuthContextType);
 
 export function AuthProvider({ children }: any) {
-  const isAuthenticated = false;
+  const [user, setUser] = useState<User | undefined>(undefined)
+
+  const isAuthenticated = !!user;
 
   const signIn = async ({ email, password }: SignInDataType) => {
-    const { token, user } = await signInRequest({ email, password })
+    const { token, user } = await signInRequest({
+      email,
+      password,   
+    })
     
     setCookie(undefined, 'nextauth.token', token, {
       maxAge: 60 * 60 * 1 //1 hour
     })
+
+    setUser(user);
+
+    Router.push('/dashboard')
   }
 
+  console.log(parseCookies(undefined, ))
+
+  useEffect(() => {
+    
+  }, [])
+
   return (
-    <AuthContext.Provider value={{ isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
       {children}
     </AuthContext.Provider>
   )
